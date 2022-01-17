73f0d20044a8fdba6183f281d8df0888ce151c0c
RuthGajj05 <ruthwika11@gmail.com>
2022-01-16 16:03:39 -0800

    Initialized MecanumAuto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RealMecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RealMecanumTeleOp.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-@TeleOp(name = "GamepadDrive1 (Blocks to Java)")
+@TeleOp(name = "Real Mecanum TeleOp")
 public class RealMecanumTeleOp extends LinearOpMode {
 
     private DcMotor TopRight;
