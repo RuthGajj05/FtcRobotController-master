7e8c1b1d8174696e770d7b3aff405b4bb8ef9023
RuthGajj05 <ruthwika11@gmail.com>
2022-01-22 12:15:52 -0800

    Initialized LinearSlidesTest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearSlidesTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearSlidesTest.java
@@ -2,8 +2,38 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-@Autonomous(name = "Slides, Intake, Carousel Test")
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name = "IntakeCarouselSlides Test")
 public class LinearSlidesTest extends LinearOpMode {
+    DcMotor carousel = null;
+    DcMotor intake = null;
+    DcMotor linearSlides = null;
+    HardwareMap hwMap = null;
+    public void init(HardwareMap Map) {
+        carousel = hwMap.get(DcMotor.class, "Carousel");
+        intake = hwMap.get(DcMotor.class, "Intake");
+        linearSlides = hwMap.get(DcMotor.class, "Linear Slides");
+
+        carousel.setDirection(DcMotor.Direction.FORWARD);
+        intake.setDirection(DcMotor.Direction.FORWARD);
+        linearSlides.setDirection(DcMotor.Direction.FORWARD);
+
+        carousel.setPower(0);
+        intake.setPower(0);
+        linearSlides.setPower(0);
+
+        carousel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        linearSlides.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        carousel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        intake.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        linearSlides.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
     CompAuto auto = new CompAuto();
     public void runOpMode() {
         intake();
