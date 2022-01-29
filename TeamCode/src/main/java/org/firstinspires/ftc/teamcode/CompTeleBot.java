4b677efd0a43549dcaedfe909a51cb64baa1614f
IshaanPitre <68621740+IshaanPitre@users.noreply.github.com>
2022-01-29 15:37:32 -0800

    Update CompTeleBot.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompTeleBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompTeleBot.java
@@ -2,15 +2,20 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.PwmControl;
+import com.qualcomm.robotcore.hardware.Servo;
 
 
-public class CompTeleBot {
+public class TerraBot {
 
     private DcMotor TopRight;
     private DcMotor TopLeft;
     private DcMotor BottomRight;
     private DcMotor BottomLeft;
-//    private DcMotor DuckSpinner;
+    private Servo Arm;
+    private DcMotor DuckSpinner;
+    private DcMotor intake;
+    private  DcMotor linearSlides;
 
     HardwareMap hwMap = null;
 
@@ -19,31 +24,51 @@ public class CompTeleBot {
         TopLeft = Map.get(DcMotor.class, "l1");
         BottomRight = Map.get(DcMotor.class, "r2");
         BottomLeft = Map.get(DcMotor.class, "l2");
+        Arm = Map.get(Servo.class, "Drop");
 
-//        DuckSpinner = Map.get(DcMotor.class, "Duck Spinner");
+        Arm.setDirection(Servo.Direction.REVERSE);
+        Arm.setPosition(0.1);
+
+
+        DuckSpinner = Map.get(DcMotor.class, "Carousel");
+        intake = Map.get(DcMotor.class, "Intake");
+        linearSlides = Map.get(DcMotor.class, "Linear Slides");
 
         TopLeft.setDirection(DcMotor.Direction.REVERSE);
         BottomRight.setDirection(DcMotor.Direction.FORWARD);
-        TopRight.setDirection(DcMotor.Direction.REVERSE);
+        TopRight.setDirection(DcMotor.Direction.FORWARD);
         BottomLeft.setDirection(DcMotor.Direction.REVERSE);
 
-//        DuckSpinner.setDirection(DcMotor.Direction.FORWARD);
+        DuckSpinner.setDirection(DcMotor.Direction.REVERSE);
+        intake.setDirection(DcMotor.Direction.REVERSE);
+        linearSlides.setDirection(DcMotor.Direction.FORWARD);
+
 
         TopLeft.setPower(0);
         BottomLeft.setPower(0);
         TopRight.setPower(0);
         BottomRight.setPower(0);
-//        DuckSpinner.setPower(0);
+        DuckSpinner.setPower(0);
+        intake.setPower(0);
+
+        linearSlides.setPower(0);
 
         TopLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         BottomLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         TopRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         BottomRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        DuckSpinner.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        linearSlides.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         TopLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         BottomLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         TopRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         BottomRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        DuckSpinner.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        intake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        linearSlides.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
 
 
 
@@ -57,6 +82,12 @@ public class CompTeleBot {
         TopLeft.setPower(power - offset);
 
     }
+    public void sides(double power){
+        TopRight.setPower(-power);
+        BottomLeft.setPower(power);
+        BottomRight.setPower(power);
+        TopLeft.setPower(-power);
+    }
     public void turn(double power){
         TopRight.setPower(power);
         BottomRight.setPower(power);
@@ -65,11 +96,29 @@ public class CompTeleBot {
     }
     public void stop(){
         move(0, 0);
-//        DuckSpinner.setPower(0);
     }
 
-//    public void duckThing(){
-//        DuckSpinner.setPower(0.5);
-//    }
+    public void servoThing(double pos){
+        Arm.setPosition(pos);
+    }
 
+    public void duckThing(){
+        DuckSpinner.setPower(0.5);
+    }
+
+    public void duckStop(){
+        DuckSpinner.setPower(0);
+    }
+
+    public void intakeM(double power){
+        intake.setPower(power);
+    }
+
+    public double armThing(){
+        return Arm.getPosition();
+    }
+// 537
+    public void linearSlidesM(double power){
+        linearSlides.setPower(power);
+    }
 }
